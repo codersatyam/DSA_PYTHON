9190125d00469ae2a247416fc7ddb0cbc1164eca
satyam <satyammittal1011@gmail.com>
2022-02-28 00:02:13 +0530

    sw1

---
+++ Sliding window/Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,11 @@
+def lengthOfLongestSubstring(self, s: str) -> int:
+        hash_map=dict()
+        window_start=0
+        max_length=0
+        for window_end in range(len(s)):
+            current_char=s[window_end]
+            if current_char in hash_map:
+                window_start=max(window_start,hash_map[current_char]+1)
+            max_length=max(max_length,window_end-window_start+1)
+            hash_map[current_char]=window_end        
+        return max_length 
